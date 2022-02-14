e2c56e6e4b0fb8d5187149d51b369d990d11a136
ASce <1587328324@qq.com>
2022-02-14 15:19:18 +0800

    配置文件

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,3 +67,31 @@ function start() {
 }
 
 start();
+
+
+
+
+
+// 深拷贝
+function deepClone(obj = {}, map = new Map()) {
+  if(typeof obj !== 'object') {
+    return obj;
+  }
+  if(map.get(obj)) {
+    return map.get(obj)
+  }
+
+  let result = {}
+  if(obj instanceof Array || Object.prototype.toString(obj) === '[object Array]') {
+    result = []
+  }
+
+  map.set(obj, result)
+  for(const key in obj) {
+    if(obj.hasOwnProperty(key)) {
+      result[key] = deepClone(obj[key], map)
+    }
+  }
+
+  return result
+}
